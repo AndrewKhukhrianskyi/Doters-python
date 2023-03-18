a435ee967d8ec57273925f5111be0c66380dd26c
AndrewKhukhrianskyi <kennyman143@gmail.com>
2023-03-18 18:03:46 +0200

    adds cw 18.03

---
+++ CW/18.03.py
@@ -0,0 +1,8 @@
+'''
+1. Решить задачу: https://www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions/python
+2. Решить задачу: https://www.codewars.com/kata/5838a66eaed8c259df000003
+3. Почитать о регулярных выражениях: https://en.wikipedia.org/wiki/Regular_expression
+4. Написать запрос регулярного выражения по следующей задаче:
+Write a Python program to check that a string contains only
+a certain set of characters (in this case a-z, A-Z and 0-9).
+'''
